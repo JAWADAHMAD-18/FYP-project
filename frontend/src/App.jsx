98eb2f88adc020cfce3441a83009e92ad832e6b5
JAWADAHMAD-18 <jawadahmad5618@gmail.com>
2025-11-08 14:25:23 +0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     {/* The `<h1 className='font-bold '>hello</h1>` code is rendering a heading element `<h1>` with the
+    text "hello" inside it. The `className='font-bold'` attribute is setting the CSS class of the
+    heading element to apply a bold font style to the text "hello".  */}
+    <h1 className='font-bold flex justify-center items-center  '>hello</h1>
+
+    </>
+  )
+}
+
+export default App
