84a27cc951071b183589fb7158a9d97208b27e3c
JAWADAHMAD-18 <jawadahmad5618@gmail.com>
2026-05-22 10:51:30 +0500

    Just correct something to make sure my app got access to apis

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    // origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin:true
   })
 );
 app.use(cookieParser());
